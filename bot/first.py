0e4ad883491f3e031887ab9b2f45beec763e6068
HusseinAbuYazeed <husseinabuyazeed@gmail.com>
2026-09-15 14:53:17 +0300

    Refactor bot initialization and add message handling capabilities

--- bot/first.py
+++ bot/first.py
@@ -3,3 +3,20 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+intents = discord.Intents.default()
+
+# to make the bot able to handle messages
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = "!", intents = intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot: {bot.user}")
+    print(f"Bot ID: {bot.user.id}")
+
+bot.run(TOKEN)
