66d82cfabd7f75c1ef1be977272caa886213052a
HusseinAbuYazeed <husseinabuyazeed@gmail.com>
2026-09-15 16:24:40 +0300

    Refactor analyze command to enhance message collection and user activity analysis

--- bot/first.py
+++ bot/first.py
@@ -1,19 +1,23 @@
 import os
 import discord
 from discord.ext import commands
-from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
 
-# to make the bot able to handle messages
+# Make the bot able to read message content
 intents.message_content = True
 
-# make a bot object
-bot = commands.Bot(command_prefix = "!", intents = intents)
+# Make a bot object
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
 
 @bot.event
 async def on_ready():
@@ -30,17 +34,61 @@ async def on_ready():
 )
 async def analyze(interaction: discord.Interaction):
 
+    # Get the current channel
     channel = interaction.channel
 
+    # Collect messages
     messages = []
 
     async for message in channel.history(limit=100):
         messages.append(message)
 
-    print(len(messages))
+    print(f"Collected {len(messages)} messages")
 
-    await interaction.response.send_message(
-        "I collected the messages!"
+    # Get usernames
+    users = []
+
+    for message in messages:
+        users.append(message.author.name)
+
+    print(users)
+
+    # Count messages for each user
+    user_frequency = {}
+
+    for user in users:
+
+        if user not in user_frequency:
+            user_frequency[user] = 1
+
+        else:
+            user_frequency[user] += 1
+
+    print(user_frequency)
+
+    # Find the most active user
+    most_active_user = max(
+        user_frequency,
+        key=user_frequency.get
     )
+
+    most_active_count = user_frequency[most_active_user]
+
+    print(
+        f"Most active user: {most_active_user}"
+    )
+
+    print(
+        f"Messages: {most_active_count}"
+    )
+
+    # Send result to Discord
+    await interaction.response.send_message(
+        f"Chat Analysis: \n"
+        f"Messages analyzed: {len(messages)}\n"
+        f"Most active user: {most_active_user}\n"
+        f"Messages: {most_active_count}"
+    )
+
 
 bot.run(TOKEN)
